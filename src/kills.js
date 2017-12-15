69ab17b96cf5659aa42d2ddcb643f16c05c349cd
Chris Lentner <cjl8505@rit.edu>
2017-12-15 15:44:34 -0500

    Add comments to kills.js

--- src/kills.js
+++ src/kills.js
@@ -1,4 +1,5 @@
 // http://stackoverflow.com/a/9517879/3707721
+// Inject a script into the current window context so it has access to the same variables
 function injectScript(f) {
     var actualCode = '(' + f + ')();';
     var script = document.createElement('script');
@@ -7,17 +8,24 @@ function injectScript(f) {
     script.parentNode.removeChild(script);
 }
 
+/*
+Wrap the global connect() function with our own logic.
+Add a new event listener to count the number of "k" type messages over the socket
+Conditionals are taken from de-minified game source.
+*/
 function addKills(){
     var oldconnect = window.connect;
     window.connect = function(){
         var res = oldconnect();
         try {
+            // We have a server to connect to
             if (0 != sos.length){
                 window.kills = 0;
 
                 window.ws.addEventListener("message", function(b){
                     if (ws == this) {
                         var c = new Uint8Array(b.data);
+                        // Logic from game.js. Pretty sure this is off-by-one.
                         if (2 <= c.length) {
                             var f = String.fromCharCode(c[2]);
                             if ("k" == f) {
@@ -33,13 +41,16 @@ function addKills(){
         return res;
     }
 
+    // Wrap redraw so we can display the count
     var oldredraw = window.redraw;
     window.redraw = function(){
         oldredraw();
+        // Conditional copied from game.js. Only periodically is the lbf display redrawn.
         if (window.animating) {
             window.lbf.innerHTML += '<br><span>Your kills: ' + window.kills + '</span>';
         }
     }
+    // Make space for it
     window.lbf.style.height = "50px";
 }
 
